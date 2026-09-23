d8bc16e2fd82f0742f38350494e98310d275a1d6
salamou1944 <salamou1944@gmail.com>
2026-09-23 19:32:02 +0100

    test(astra): strengthen Kraken execution safety tests

--- tests/test_kraken_execution.py
+++ tests/test_kraken_execution.py
@@ -1,7 +1,5 @@
-import os
 import unittest
 from astra.execution.kraken import KrakenExecutionConfig, KrakenExecutionError, KrakenSpotExecutor
-
 
 class KrakenExecutionSafetyTests(unittest.TestCase):
     def test_default_is_off(self):
@@ -24,6 +22,17 @@ class KrakenExecutionSafetyTests(unittest.TestCase):
     def test_withdrawal_capability_does_not_exist(self):
         self.assertFalse(hasattr(KrakenSpotExecutor, "withdraw"))
 
+    def test_private_methods_are_blocked_when_off(self):
+        ex = KrakenSpotExecutor(KrakenExecutionConfig(api_key="", api_secret=""))
+        for method, args in [
+            ("query_balance", {}),
+            ("query_open_orders", {}),
+            ("query_orders", {}),
+            ("cancel_order", {"txid": "x"}),
+        ]:
+            with self.subTest(method=method):
+                with self.assertRaises(KrakenExecutionError):
+                    getattr(ex, method)(**args)
 
 if __name__ == "__main__":
     unittest.main()
